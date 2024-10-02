5e15115484367854ef187e5789782cad5017fc97
charlieolinsky <charlesolinsky@gmail.com>
2024-10-02 16:51:54 -0400

    added delete to tree

--- pkg/ds/tree.go
+++ pkg/ds/tree.go
@@ -1,5 +1,9 @@
 package ds
 
+import (
+	"errors"
+)
+
 type TreeNode struct {
 	Value    int
 	Parent   *TreeNode
@@ -32,7 +36,7 @@ func (t *Tree) Insert(parent *TreeNode, val int) *TreeNode {
 	return node
 }
 
-// Return true or false if the specified value exists in the true.
+// Return true or false if the specified value exists in the tree.
 // Recursive DFS: Worst-case Time O(n) where n is the number of nodes in the tree. Worse-case Space O(d) where d is the depth of the tree
 func (t *Tree) Contains(root *TreeNode, val int) bool {
 	//Base case: If the current node is nil, return false
@@ -54,4 +58,38 @@ func (t *Tree) Contains(root *TreeNode, val int) bool {
 
 	//If no match is found in the current node or its children, return false
 	return false
+}
+
+// Delete the first occurrence of the provided value in the tree or return error
+// Recursive DFS: time O(n), worst Case Space O(n), average case space O(log(n))
+func (t *Tree) Delete(root *TreeNode, val int) error {
+	if root == nil {
+		return errors.New("cannot delete from an empty tree")
+	}
+
+	// Traverse the children to find the node with the value
+	for i := 0; i < len(root.Children); i++ {
+
+		//Current Node (potential node to be deleted)
+		cur := root.Children[i]
+
+		if cur.Value == val {
+			// take a slice of Children up to but not including cur (i), then append all of cur's children to the root's children slice.
+			//This effectively deletes cur and adds its children in its place
+			root.Children = append(root.Children[:i], cur.Children...)
+
+			// Update parent pointers of promoted nodes. Once cur is deleted is children are still pointing at it.
+			for _, child := range cur.Children {
+				child.Parent = root
+			}
+			return nil
+		}
+
+		// Recursively search in cur's children
+		if err := t.Delete(cur, val); err == nil {
+			return nil
+		}
+	}
+
+	return errors.New("provided value does not exist in the tree")
 }
